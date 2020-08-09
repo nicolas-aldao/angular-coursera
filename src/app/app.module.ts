084ddab80b18b63b29530e7d55d87ce73e3aa269
Nicolás Aldao <nicolasaldao@live.com>
2020-08-09 15:44:16 -0300

    Menu component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // agregamos el import
+import { MatToolbarModule } from '@angular/material/toolbar'; // agregamos el import
+import { FlexLayoutModule } from '@angular/flex-layout'; // agregamos el import
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs'; // agregamos el import
+import { MenuComponent } from './menu/menu.component'; 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, // agregamos las variables
+    MatToolbarModule, // agregamos las variables
+    FlexLayoutModule, // agregamos las variables
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
